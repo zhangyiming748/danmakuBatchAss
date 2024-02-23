fba0d5db4fc2a4f45b520ebcd890b628ffe3bc2e
zen <zhangyiming748@gmail.com>
2024-02-23 16:04:30 +0800

    fix

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import "github.com/zhangyiming748/GetFileInfo"
 func main() {
 	var root string
 	if runtime.GOOS == "android" && runtime.GOARCH == "arm64" {
-		root = "sdcard/Movies/bili"
+		root = "/sdcard/Movies/bili"
 	}
 	files := GetFileInfo.GetAllFilesInfo(root, "xml")
 	for _, file := range files {
